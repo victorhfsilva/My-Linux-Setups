47eb8ee1beeb787b37891ec365b7f7ddd622c20f
Victor <victorhfsilva@protonmail.com>
2022-09-06 19:44:08 -0300

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# My Linux Setups
+
+This repository describe the process of installation and configuration of the softwares I current use on Linux.
---
+++ ubuntu-setup.sh
@@ -0,0 +1,116 @@
+#! usr/bin/bash
+
+########
+# IDEs #
+########
+
+#Install JDK
+sudo apt install default-jdk -y
+
+#Install Netbeans
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu focal universe" -y
+sudo apt update
+sudo apt install netbeans -y
+
+#Install Arduino IDE
+sudo apt install arduino -y
+
+#Install Code::Blocks
+sudo apt install codeblocks -y
+
+########
+# Text #
+########
+
+#Install Calibre
+sudo apt install calibre -y
+
+#Install Ghostwriter
+sudo apt install ghostwriter -y
+
+#Install PDF Mix Tool
+sudo snap install pdfmixtool
+
+############
+# Browsers #
+############
+
+#Install Google Chrome
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update
+sudo apt install google-chrome-stable -y
+
+############
+# Graphics #
+############
+
+#Install Darktable
+sudo apt install darktable -y
+
+#Install Gimp
+sudo apt install gimp -y
+
+#Install Gpick
+sudo apt install gpick -y
+
+#Install Gthumb
+sudo apt install gthumb -y
+
+#Install Inkscape
+sudo apt install inkscape -y
+
+#################
+# Communication #
+#################
+
+#Install Discord
+sudo snap install discord
+
+#Install Telegram
+sudo apt install telegram-desktop -y
+
+############
+# Networks #
+############
+
+#Install GNS3
+sudo apt install gns3-iou -y
+
+#Install Wireshark
+sudo apt install wireshark -y
+
+#########
+# Tools #
+#########
+
+#Install Gparted
+sudo apt install gparted -y
+
+#Install VirtualBox
+sudo apt install virtualbox -y
+
+#Install Gdebi
+sudo apt install gdebi -y
+
+#Install Pip
+sudo apt install python3-pip -y
+
+#Install Youtube-DL
+sudo pip3 install --upgrade youtube_dl
+
+#Install Git
+sudo apt install git -y
+
+#########
+# Vídeo #
+#########
+
+#Install OBS Studio
+sudo apt install obs-studio -y
+
+
+
+
+
+
