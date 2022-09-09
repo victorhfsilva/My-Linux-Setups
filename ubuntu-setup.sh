8bf9954f2ef97017b21b560cc0c7936320d4efd8
Victor <victorhfsilva@protonmail.com>
2022-09-09 09:53:47 -0300

    Add Databases installation section

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -1,8 +1,8 @@
 #! usr/bin/bash
 
-########
-# IDEs #
-########
+#
+# IDEs 
+#
 
 #Install JDK
 sudo apt install default-jdk -y
@@ -15,12 +15,9 @@ sudo apt install netbeans -y
 #Install Arduino IDE
 sudo apt install arduino -y
 
-#Install Code::Blocks
-sudo apt install codeblocks -y
-
-########
-# Text #
-########
+#
+# Text 
+#
 
 #Install Calibre
 sudo apt install calibre -y
@@ -31,9 +28,9 @@ sudo apt install ghostwriter -y
 #Install PDF Mix Tool
 sudo snap install pdfmixtool
 
-############
-# Browsers #
-############
+#
+# Browsers 
+#
 
 #Install Google Chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
@@ -41,9 +38,9 @@ sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable
 sudo apt update
 sudo apt install google-chrome-stable -y
 
-############
-# Graphics #
-############
+#
+# Graphics 
+#
 
 #Install Darktable
 sudo apt install darktable -y
@@ -60,9 +57,9 @@ sudo apt install gthumb -y
 #Install Inkscape
 sudo apt install inkscape -y
 
-#################
-# Communication #
-#################
+#
+# Communication 
+#
 
 #Install Discord
 sudo snap install discord
@@ -70,9 +67,9 @@ sudo snap install discord
 #Install Telegram
 sudo apt install telegram-desktop -y
 
-############
-# Networks #
-############
+#
+# Networks 
+#
 
 #Install GNS3
 sudo apt install gns3-iou -y
@@ -80,9 +77,9 @@ sudo apt install gns3-iou -y
 #Install Wireshark
 sudo apt install wireshark -y
 
-#########
-# Tools #
-#########
+#
+# Tools 
+#
 
 #Install Gparted
 sudo apt install gparted -y
@@ -105,9 +102,21 @@ sudo apt install git -y
 #Install Curl
 sudo apt install curl -y
 
-#########
-# Vídeo #
-#########
+#
+# Databases 
+#
+
+#Install PostgreSQL
+sudo apt install postgresql
+
+#Install pgAdmin
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4
+
+#
+# Vídeo 
+#
 
 #Install OBS Studio
 sudo apt install obs-studio -y
